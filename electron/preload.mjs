445d9ba583291df4f4789bf366e3909a0e6efa3b
JSR <pandajsr72@gmail.com>
2026-04-01 21:01:00 +0800

    chore: 移除未使用的 preload.mjs

--- electron/preload.mjs
+++
@@ -1,7 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron'
-
-contextBridge.exposeInMainWorld('eazyFiiDesktop', {
-  pickOpenDirectory: () => ipcRenderer.invoke('desktop:pick-open-directory'),
-  pickSaveDirectory: () => ipcRenderer.invoke('desktop:pick-save-directory'),
-  writeProjectFiles: (payload) => ipcRenderer.invoke('desktop:write-project-files', payload),
-})
